da0a8587e3b8a67ffd0303fc740d5cdfc7c6f917
Evelyn Wu <evelynwu@Mac.modem>
2025-03-16 20:46:13 +1100

    learning notes

--- Course materials/04_01_Creating_Arrays/app.js
+++ Course materials/04_01_Creating_Arrays/app.js
@@ -9,3 +9,24 @@ let lotto = [ 45, 12, 23, 25, 34 ];
 
 // Array of multiple types:
 let myCollection = [ 12, 'dog', true, null, NaN ];
+
+let subreddits = ['soccer', 'popheads', 'cringe', 'books'];
+for (let sub of subreddits) {
+    console.log(sub);
+}
+
+const magicSquare = [
+    [2, 7, 6],
+    [9, 5, 1],
+    [4, 3, 8]
+];
+
+for (let i = 0; i < magicSquare.length; i++) {
+    let row = magicSquare[i];
+    let sum = 0;
+    for (let j = 0; j < row.length; j++) {
+        // console.log(row[j]);
+        sum += row[j];
+    }
+    console.log(sum);
+}
